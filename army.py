922938d21010cb63aecbbda76d30ee1c95ef9a9d
Wan <wnh3yang@gmail.com>
2020-06-28 18:33:33 +0800

    模拟按键

--- army.py
+++ army.py
@@ -1,23 +1,25 @@
 import asyncio
+from pynput.keyboard import Controller
 
 from core import BaseAsyncBot
+
+
+keyboard = Controller()
 
 
 class ZhuDongSanGuang(BaseAsyncBot):
     """主动三光"""
 
-    async def task_press_1(self):
-        while self.is_run:
-            await asyncio.sleep(0.1)
-            print("按下了1")
-            await asyncio.sleep(0.112)
-            print("提起了1")
-            await asyncio.sleep(0.3)
-
     async def task_press_2(self):
         while self.is_run:
-            await asyncio.sleep(0.2)
-            print("按下了2")
-            await asyncio.sleep(0.112)
-            print("提起了2")
-            await asyncio.sleep(0.7)
+            await asyncio.sleep(0.11)
+            keyboard.press('2')
+            await asyncio.sleep(0.172)
+            keyboard.release('2')
+
+    async def task_press_4(self):
+        while self.is_run:
+            await asyncio.sleep(0.12)
+            keyboard.press('4')
+            await asyncio.sleep(0.212)
+            keyboard.release('4')
